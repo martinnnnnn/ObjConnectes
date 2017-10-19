316f98d4f507c914a73d84061620b544a38ebbaa
Léo Piérot <leo.fpierot@gmail.com>
2017-10-19 10:57:54 +0200

    lvl 2 : - Construction d'un pattern de fleur et reconnaissance du pattern par les autres fleurs à l'écran - perte de pétales décrite par le gdd

---
+++ Assets/_assets/2.scripts/2.Gameplay/Lvl2/PatternFlower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternFlower : Flower {
+
+    [SerializeField]
+    private int m_NumberOfPetalsUp = 5;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RollPetalLoss()
+    {
+        List<Petal> PetalsCopy = new List<Petal>();
+        foreach(Petal petal in Petals)
+        {
+            PetalsCopy.Add(petal);
+        }
+        PetalsCopy.Shuffle();
+
+        while(m_PetalsUpCount > m_NumberOfPetalsUp)
+        {
+            PetalsCopy[0].Leave();
+            PetalsCopy.RemoveAt(0);
+        }
+    }
+}
